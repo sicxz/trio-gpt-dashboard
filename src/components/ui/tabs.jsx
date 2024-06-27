1d5f8aa7091aabc47193ebaa4a4210b94ba850fc
tmasingale <4572430+tmasingale@users.noreply.github.com>
2024-06-27 15:46:17 -0700

    Update dashboard with working UI components

---
+++ src/components/ui/tabs.jsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
+
+const Tabs = TabsPrimitive.Root
+const TabsList = TabsPrimitive.List
+const TabsTrigger = TabsPrimitive.Trigger
+const TabsContent = TabsPrimitive.Content
+
+export { Tabs, TabsList, TabsTrigger, TabsContent }
